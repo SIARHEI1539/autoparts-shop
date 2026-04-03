48f86153367ef92f3f7607e68a33f1b0988c4de0
Siarhei Koliadich <Koliaditch.sergey@yandex.ru>
2026-04-03 10:37:14 +0300

    Add Part model to INSTALLED_APPS and create superuser

--- backend/parts/admin.py
+++ backend/parts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Part
 
-# Register your models here.
+@admin.register(Part)
+class PartAdmin(admin.ModelAdmin):
+    list_display = ('name', 'sku', 'price', 'stock', 'category')
+    list_filter = ('category', 'manufacturer')
+    search_fields = ('name', 'sku', 'description')
