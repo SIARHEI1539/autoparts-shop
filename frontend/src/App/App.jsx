ede8b33b3a15758ad9bcca86deab9e6ead4e0ee9
Siarhei Koliadich <Koliaditch.sergey@yandex.ru>
2026-04-11 21:21:42 +0300

    Создал Header и Footer

---
+++ frontend/src/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+import PartsList from '../components/PartsList/PartsList';
+import './App.css';
+
+function App() {
+  return (
+    <Layout>
+      <PartsList />
+    </Layout>
+  );
+}
+
+export default App;
