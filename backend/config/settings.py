d918ca55ea8bc0ec7f6ff3f0f5050f2dcf346a45
Siarhei Koliadich <Koliaditch.sergey@yandex.ru>
2026-04-11 12:02:52 +0300

    Подключил React

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -126,7 +126,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",   # React при локальной разработке
-    "http://127.0.0.1:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",  # Порт React-приложения
+# ]
